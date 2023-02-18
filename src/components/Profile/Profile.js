8b3f8e7f1343336aa92bb41465fa7ad391a10c1d
Vipa4ka <vipa4ka@gmail.com>
2023-02-18 18:05:35 +0200

    finish Profile task

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+import defaultAvatar from "./avatarka.jpg";
 import styles from "./Profile.module.css";
 
 const Profile = ({ name, tag, location, followers, views, likes, avatar }) => {
@@ -26,6 +28,20 @@ const Profile = ({ name, tag, location, followers, views, likes, avatar }) => {
       </ul>
     </div>
   );
+};
+
+Profile.defaultProps = {
+  avatar: defaultAvatar,
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+  avatar: PropTypes.string,
 };
 
 export default Profile;
